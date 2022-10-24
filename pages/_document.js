7910389a4152970d1db9b9c9aef144df07c4ca04
xiownthispla <xiownthisplacex@hotmail.com>
2022-10-24 16:45:06 -0500

    modified:   package.json 	modified:   pages/_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,15 @@
-//import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-//export default function Document() {
-//    return (
-//        <Html>
-//            <Head />
-//            <body>
-//                {/* Chakra ui color script mode */}
-
-//                <Main />
-//                <NextScript />
-//            </body>
-//        </Html>
-//    );
-//}
-
-import Document, { Html, Head, Main, NextScript } from "next/document";
-
-class MainDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
+export default function Document() {
     return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          {/*Below we add the modal wrapper*/}
-          <div id="modal-root"></div>
-        </body>
-      </Html>
-    );
-  }
-}
+        <Html>
+            <Head />
+            <body>
+                {/* Chakra ui color script mode */}
 
-export default MainDocument;
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
